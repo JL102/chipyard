c4139993ef96f9345be2d62bfa07739bdb7006b8
Jerry Zhao <jerryz123@berkeley.edu>
2023-03-06 15:50:25 -0800

    Add rocc-to-noc example

--- generators/chipyard/src/main/scala/config/NoCConfigs.scala
+++ generators/chipyard/src/main/scala/config/NoCConfigs.scala
@@ -243,3 +243,24 @@ class AMMConfigSimple extends Config(
   new freechips.rocketchip.subsystem.WithNBigCores(1) ++
   new chipyard.config.AbstractConfig
   )
+class RoCCOverNoCConfig extends Config(
+  new chipyard.example.WithRoNNoC(chipyard.example.RoNNoCProtocolParams(
+    hartMappings = ListMap( // naively map hartIds to the same nodeId
+      0 -> 0,
+      1 -> 1,
+      2 -> 2,
+      3 -> 3,
+      4 -> 4,
+      5 -> 5,
+      6 -> 6,
+      7 -> 7),
+    nocParams = NoCParams(
+      topology = Mesh2D(2, 4),
+      channelParamGen = (a, b) => UserChannelParams(Seq.fill(3) { UserVirtualChannelParams(2) }), // 3 VCs/channel, 2 buffer slots/VC
+      routingRelation = Mesh2DDimensionOrderedRouting()
+    )
+  )) ++
+  new chipyard.example.WithRoCCToNoC ++
+  new freechips.rocketchip.subsystem.WithNBigCores(8) ++
+  new chipyard.config.AbstractConfig
+)
